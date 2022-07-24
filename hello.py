f0b83add6b42002c8111f1918e2e1ee12b53d52b
issaod23-meet <109899312+issaod23-meet@users.noreply.github.com>
2022-07-24 13:32:53 +0300

    Create hello.py
    first lab is doneee

---
+++ hello.py
@@ -0,0 +1,4 @@
+
+print("hello world")
+print('hey my name is Issa Odeh, Im 16, and I have been playing the guitar for 9 years. also im a swimmer in the palestinian national team')
+print('eager to meet you all, and get better at cs. Hopefully XD!')
